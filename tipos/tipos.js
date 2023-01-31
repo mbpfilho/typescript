10d73bf7f922571126d2af02743ea6a9aa890bb5
Marcio Bueno <mabuen@live.com>
2023-01-31 10:41:10 +0100

    tipos e tuplas

---
+++ tipos/tipos.js
@@ -0,0 +1,32 @@
+"use strict";
+//string
+let nome = "Joäo";
+console.log(nome);
+// nome=28
+//number
+let idade = 27;
+// idade="Ana"
+idade = 27.4;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+// possuiHobbies=1
+console.log(possuiHobbies);
+//tipos explícitos
+let minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade="idade é 27"
+// console.log(typeof minhaIdade)
+//array
+let hobbies = ["cozinhar", "esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 200, 300];
+// hobbies=100
+console.log(hobbies);
+//tuplas
+let endereco = ["Av Principal", 99, ""];
+console.log(endereco);
+endereco = ["Rua Importante", 1260, "Bloco C"];
+console.log(endereco);
---
+++ tipos/tipos.ts
@@ -0,0 +1,38 @@
+//string
+let nome="Joäo"
+console.log(nome)
+// nome=28
+
+//number
+let idade=27
+// idade="Ana"
+idade=27.4
+console.log(idade)
+
+//boolean
+let possuiHobbies=false
+// possuiHobbies=1
+console.log(possuiHobbies)
+
+//tipos explícitos
+let minhaIdade: number
+minhaIdade=27
+console.log(typeof minhaIdade)
+// minhaIdade="idade é 27"
+// console.log(typeof minhaIdade)
+
+//array
+let hobbies:any[]=["cozinhar","esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies=[100,200,300]
+// hobbies=100
+console.log(hobbies)
+
+//tuplas
+let endereco:[string,number,string]=["Av Principal",99,""] 
+console.log(endereco)
+
+endereco=["Rua Importante", 1260,"Bloco C"]
+console.log(endereco)
