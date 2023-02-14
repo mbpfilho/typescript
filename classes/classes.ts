25fa1b2cc30fcef20126584cfc6283942c502ab7
Marcio Bueno <mabuen@live.com>
2023-02-14 11:51:59 +0100

    modificadores de acesso

--- classes/classes.ts
+++ classes/classes.ts
@@ -50,3 +50,36 @@ prod1.desconto=.06
 console.log(prod1.resumo())
 const prod2=new Produto("feijäo",25,.1)
 console.log(prod2.resumo())
+
+class Carro{
+  private velocidadeAtual: number =0
+
+  constructor(public marca:string, public modelo: string, private velocidadeMaxima: number =200){
+
+  }
+
+  private alterarVelocidade(delta:number):number{
+    const novaVelocidade=this.velocidadeAtual + delta
+    const velocidadeValida = novaVelocidade >=0 && novaVelocidade<= this.velocidadeMaxima
+
+    if(velocidadeValida) this.velocidadeAtual=novaVelocidade
+    else this.velocidadeAtual=delta>0?this.velocidadeMaxima:0
+
+    return this.velocidadeAtual
+  }
+
+  public acelerar():number {
+    return this.alterarVelocidade(5)
+  }
+
+  public frear():number {
+    return this.alterarVelocidade(-5)
+  }
+}
+
+const carro1=new Carro("Ford", "Ka", 185)
+Array(50).fill(0).forEach(()=>carro1.acelerar())
+console.log(carro1.acelerar())
+console.log(carro1.frear())
+Array(30).fill(0).forEach(()=>carro1.frear())
+console.log(carro1.frear( )) 
