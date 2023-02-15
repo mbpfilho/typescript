3f10a9011fe9e016da73db790f48950a3c6a0107
Marcio Bueno <mabuen@live.com>
2023-02-15 12:19:42 +0100

    namespaces

---
+++ namespaces/namespaces.js
@@ -0,0 +1,18 @@
+"use strict";
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function circunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circunferencia = circunferencia;
+        function retangulo(base, altura) {
+            return base * altura;
+        }
+        Area.retangulo = retangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
