3c3f5910a2a4d40d49f605654407d3226b3dfdb7
Marcio Bueno <mabuen@live.com>
2023-02-15 16:03:52 +0100

    namespaces

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,18 +1,5 @@
 "use strict";
-var Geometria;
-(function (Geometria) {
-    let Area;
-    (function (Area) {
-        const PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
+///<reference path="geometriaCirculo.ts"/>
+///<reference path="geometriaRetangulo.ts"/>
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,16 +1,5 @@
-namespace Geometria {
-  export namespace Area {
-    const PI =3.14
-
-    export function circunferencia(raio: number): number {
-      return PI * Math.pow(raio, 2)
-    }
-
-    export function retangulo(base: number, altura: number): number {
-      return base * altura
-    }
-  }
-}
+///<reference path="geometriaCirculo.ts"/>
+///<reference path="geometriaRetangulo.ts"/>
 
 console.log(Geometria.Area.circunferencia(10))
-console.log(Geometria.Area.retangulo(10, 20))
+console.log(Geometria.Area.retangulo(10, 20)) 
